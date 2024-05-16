bef89845baf59c938b8337377b1e41a3acf40dfd
Benjamin GRULOIS <bgrulois@bess-f3r4s12.clusters.42paris.fr>
2024-05-16 14:32:10 +0200

    because vms are bothering me

---
+++ host_setup.sh
@@ -0,0 +1,21 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install wget nano git -y
+
+#docker stuff
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+#install latest
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+ssh-keygen -t rsa
+
+echo "HOST IS SET\n"
