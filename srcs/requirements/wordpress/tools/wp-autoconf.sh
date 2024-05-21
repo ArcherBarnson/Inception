b9129acb8f904dfb2c50de281065d9f6b84af077
Benjamin GRULOIS <bgrulois@bess-f1r2s2.clusters.42paris.fr>
2024-05-21 12:10:13 +0200

    wip ca devient relou

--- srcs/requirements/wordpress/tools/wp-autoconf.sh
+++ srcs/requirements/wordpress/tools/wp-autoconf.sh
@@ -4,4 +4,8 @@ sed -i "s/DB_USER/$DB_ADMIN/g" /var/www/wordpress/wp-config-smaple.php
 sed -i "s/DB_PASSWORD/$DB_ADMIN_PW/g" /var/www/wordpress/wp-config-smaple.php
 sed -i "s/DB_HOST/$DB_HOSTNAME/g" /var/www/wordpress/wp-config-smaple.php
 
-#
+#default config setup (i think)
+wp core install --allow-root url=localhost:9000 --title=bgrulois_inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --path='/var/www/wordpress'
+
+#adding normal user 1
+wp user create --allow-root $WP_USER1 $WP_USER1_EMAIL --user_password=$WP_USER1_PASS --path='/var/www/wordpress'
