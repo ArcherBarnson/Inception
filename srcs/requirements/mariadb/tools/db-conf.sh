20c0b0d80b97b8bc6d40ad90dbfffe26c9507efc
Benjamin GRULOIS <bgrulois@bess-f2r5s2.clusters.42paris.fr>
2024-05-21 17:05:35 +0200

    ahahahahahahaghahahahahahaha

--- srcs/requirements/mariadb/tools/db-conf.sh
+++ srcs/requirements/mariadb/tools/db-conf.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-mysql -e aaaaaaaaaaaaaa
+service mysql start
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+exec mysqld_safe
--- srcs/requirements/wordpress/tools/wp-autoconf.sh
+++ srcs/requirements/wordpress/tools/wp-autoconf.sh
@@ -1,8 +1,8 @@
 #setting up wp admin user
-sed -i "s/DB_NAME/$DB_NAME/g" /var/www/wordpress/wp-config-smaple.php
-sed -i "s/DB_USER/$DB_ADMIN/g" /var/www/wordpress/wp-config-smaple.php
-sed -i "s/DB_PASSWORD/$DB_ADMIN_PW/g" /var/www/wordpress/wp-config-smaple.php
-sed -i "s/DB_HOST/$DB_HOSTNAME/g" /var/www/wordpress/wp-config-smaple.php
+sed -i "s/DB_NAME/$WP_NAME/g" /var/www/wordpress/wp-config-smaple.php
+sed -i "s/DB_USER/$WP_ADMIN/g" /var/www/wordpress/wp-config-smaple.php
+sed -i "s/DB_PASSWORD/$WP_ADMIN_PW/g" /var/www/wordpress/wp-config-smaple.php
+sed -i "s/DB_HOST/$WP_HOSTNAME/g" /var/www/wordpress/wp-config-smaple.php
 
 #default config setup (i think)
 wp core install --allow-root url=localhost:9000 --title=bgrulois_inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --path='/var/www/wordpress'
