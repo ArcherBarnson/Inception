24d353dc85fe6b983a50b9e9c4feb197d7b573e7
ArcherBarnson <barnsonarcher@gmail.com>
2024-06-06 13:00:50 -0400

    FIX .env and tools files

--- srcs/requirements/mariadb/tools/db-conf.sh
+++ srcs/requirements/mariadb/tools/db-conf.sh
@@ -2,13 +2,14 @@
 
 service mariadb start
 
-sleep 3
+#mysqld_safe &
 
-mysqladmin -u root password ${SQL_PASSWORD}
+#killall mysqld_safe
+sleep 30
+
+mysqladmin -u root password ${SQL_ROOT_PASSWORD}
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-
-echo ${SQL_DATABASE}
 
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
@@ -16,8 +17,9 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' I
 
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p ${SQL_PASSWORD} shutdown
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 
+#killall mysqld_safe
 sleep 3
 
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/wp-autoconf.sh
+++ srcs/requirements/wordpress/tools/wp-autoconf.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-if [ ! -e /var/wwww/wordpress/wp-config.php ]; then
+sleep 30
+
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
 
 #generate cfg file
-#wp config create	--allow-root \
-#			--dbname=$SQL_DATABASE \
+#wp config create	--dbname=$SQL_DATABASE \
 #			--dbuser=$SQL_USER \
 #			--dbpass=$SQL_PASSWORD \
-#			--dbhost=${SQL_DATABASE}:3306 \
-#			--path='/var/www/wordpress'
+#			--dbhost="mariadb:3306" \
+#			--path='/var/www/wordpress' \
+#			--allow-root
 
 #setting up wp admin user
 
@@ -20,14 +22,14 @@ sed -i "s/database_name_here/$SQL_DATABASE/g" /var/www/wordpress/wp-config-sampl
 sed -i "s/username_here/$SQL_USER/g" /var/www/wordpress/wp-config-sample.php
 sed -i "s/password_here/$SQL_PASSWORD/g" /var/www/wordpress/wp-config-sample.php
 sed -i "s/localhost/$SQL_HOST/g" /var/www/wordpress/wp-config-sample.php
-mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
 cat /var/www/wordpress/wp-config.php
 
 #default config setup (i think)
-wp core install --allow-root url=${SQL_DATABASE}:3306 --title=bgrulois_inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --skip-email --path='/var/www/wordpress'
+wp core install --allow-root url=localhost --title=bgrulois_inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --skip-email --path='/var/www/wordpress'
 
 #adding normal user 1
-wp user create --allow-root $WP_USER1 $WP_USER1_EMAIL --user_password=$WP_USER1_PW --path='/var/www/wordpress'
+wp user create --allow-root $WP_USER1 $WP_USER1_EMAIL --role=author --user_pass=$WP_USER1_PW --path='/var/www/wordpress'
 
 fi
