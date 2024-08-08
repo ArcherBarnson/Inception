1e397efbd800fb886a24c7b115f19bf82abafd40
ArcherBarnson <barnsonarcher@gmail.com>
2024-08-08 12:12:54 -0400

    adminer wip

--- srcs/bonus/adminer/conf/adminer_autoconf.sh
+++ srcs/bonus/adminer/conf/adminer_autoconf.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -P /var/www/wordpress/
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -P /var/www/html/wordpress/
 
 echo "Adminer Ready."
+
+php -S [::]:9001 -t /var/www/html/wordpress
