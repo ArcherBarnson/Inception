dc44124e6166b0e3f3d2b6464ce5efa0f20e8b6a
ArcherBarnson <barnsonarcher@gmail.com>
2024-08-09 06:07:31 -0400

    adminer up, cannot login because of session shenaningans

--- srcs/bonus/adminer/conf/adminer_autoconf.sh
+++ srcs/bonus/adminer/conf/adminer_autoconf.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -P /var/www/html/wordpress/
+#wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -P /var/www/html/wordpress/
+
+wget "http://www.adminer.org/latest.php" -O /var/www/wordpress/adminer.php
+
+#chown -R www-data:www-data /var/www/wordpress/adminer.php
+
+chmod 755 /var/www/wordpress/adminer.php
+
+php -S [::]:9001 -t /var/www/
 
 echo "Adminer Ready."
 
-php -S [::]:9001 -t /var/www/html/wordpress
+
