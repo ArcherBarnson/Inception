9092b1659b0b9d816de6873cd14960258bd448f9
ArcherBarnson <barnsonarcher@gmail.com>
2024-08-13 13:05:13 -0400

    up, makefile up, adminer bugged out but up, getting jaded now. . .

--- srcs/bonus/adminer/conf/adminer_autoconf.sh
+++ srcs/bonus/adminer/conf/adminer_autoconf.sh
@@ -10,6 +10,22 @@ chmod 755 /var/www/wordpress/adminer.php
 
 #$ awk '/@ini_set('session.use_trans_sid', '0');/ { print; print  "@ini_set('session.gc_maxlifetime',1440); ;" next }1' foo.input
 
-php -S [::]:9001 -t /var/www/wordpress
+if [ ! -e /run/php ]; then 
+mkdir -p /run/php
+fi
+
+#echo "Launching php fpm..."
+
+#if [ -e /etc/php/7.4/fpm/pool.d/www.conf ]; then
+#echo "www.conf EXISTS"
+#fi
+
+#if [ ! -e /var/lib/php/session ]; then 
+#mkdir -p /var/lib/php/session
+#fi
+
+chmod 777 /var/lib/php/session
 
 echo "Adminer Ready."
+
+/usr/sbin/php-fpm7.4 -F #-S [::]:9001 -t /var/www/wordpress
